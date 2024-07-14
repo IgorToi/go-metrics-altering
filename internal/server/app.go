ed500b0b3096481f90c63cdd953ad5c2d79e6b31
Igor Toigildin <toygildinigor@gmail.com>
2024-07-14 20:32:09 +0300

    Add psql db implementation

---
+++ internal/server/app.go
@@ -0,0 +1,241 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	config "github.com/igortoigildin/go-metrics-altering/config/server"
+	"github.com/igortoigildin/go-metrics-altering/internal/logger"
+	"github.com/igortoigildin/go-metrics-altering/internal/models"
+	"github.com/igortoigildin/go-metrics-altering/internal/storage"
+	"github.com/igortoigildin/go-metrics-altering/templates"
+	"go.uber.org/zap"
+)
+
+type app struct {
+    storage storage.Storage
+}
+
+func newApp(s storage.Storage) *app {
+    return &app{storage: s}
+}
+
+func routerDB(ctx context.Context, cfg *config.ConfigServer) chi.Router {
+    repo := storage.InitPostgresRepo(ctx, cfg)
+    app := newApp(repo)
+    t = templates.ParseTemplate()
+    r := chi.NewRouter()
+    r.Get("/ping", WithLogging(gzipMiddleware(http.HandlerFunc(repo.Ping))))
+    r.Get("/", WithLogging(gzipMiddleware(http.HandlerFunc(app.getAllmetrics))))
+    r.Post("/update/", WithLogging(gzipMiddleware(http.HandlerFunc(app.updateMetric))))
+    r.Post("/updates/", WithLogging(gzipMiddleware(http.HandlerFunc(app.updates))))
+    r.Post("/value/", WithLogging(gzipMiddleware(http.HandlerFunc(app.getMetric))))
+    return r
+}
+
+func (app *app) updates(w http.ResponseWriter, r *http.Request) {
+    ctx, cancel := context.WithCancel(r.Context())
+    defer cancel()
+    if r.Method != http.MethodPost {
+        logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+    }
+    var metrics []models.Metrics
+    dec := json.NewDecoder(r.Body)
+    if err := dec.Decode(&metrics); err != nil {
+        logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    for _, metric := range metrics {
+        if metric.MType != config.GaugeType && metric.MType != config.CountType {
+            logger.Log.Debug("usupported request type", zap.String("type", metric.MType))
+            w.WriteHeader(http.StatusUnprocessableEntity)
+            return
+        }
+        switch metric.MType {
+        case config.GaugeType:
+            if app.storage.Exist(ctx, metric.MType, metric.ID) {
+                err := app.storage.Update(ctx, metric.MType, metric.ID, metric.Value)
+                if err != nil {
+                    logger.Log.Debug("error while updating value", zap.Error(err))
+                    w.WriteHeader(http.StatusInternalServerError)
+                    return
+                }
+            } else {
+                err := app.storage.Add(ctx, metric.MType, metric.ID, metric.Value)
+                if err != nil {
+                    logger.Log.Debug("error while adding value", zap.Error(err))
+                    w.WriteHeader(http.StatusInternalServerError)
+                    return
+                }
+            }
+        case config.CountType:
+            if app.storage.Exist(ctx, metric.MType, metric.ID) {
+                err := app.storage.Update(ctx, metric.MType, metric.ID, metric.Delta)
+                if err != nil {
+                    logger.Log.Debug("error while updating value", zap.Error(err))
+                    w.WriteHeader(http.StatusInternalServerError)
+                    return
+                }
+            } else {
+                err := app.storage.Add(ctx, metric.MType, metric.ID, metric.Delta)
+                if err != nil {
+                    logger.Log.Debug("error while adding value", zap.Error(err))
+                    w.WriteHeader(http.StatusInternalServerError)
+                    return
+                }
+            }
+        }
+    }
+    w.Header().Set("Content-Type", "application/json")
+    logger.Log.Debug("sending HTTP 200 response")
+}
+
+func (app *app) updateMetric(w http.ResponseWriter, r *http.Request) {
+    ctx, cancel := context.WithCancel(r.Context())
+    defer cancel()
+
+    if r.Method != http.MethodPost {
+        logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+    }
+    var req models.Metrics
+    dec := json.NewDecoder(r.Body)
+    if err := dec.Decode(&req); err != nil {
+        logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    if req.MType != config.GaugeType && req.MType != config.CountType {
+        logger.Log.Debug("usupported request type", zap.String("type", req.MType))
+        w.WriteHeader(http.StatusUnprocessableEntity)
+        return
+    }
+    switch req.MType {
+    case config.GaugeType:
+        if app.storage.Exist(ctx, req.MType, req.ID) {
+            err := app.storage.Update(ctx, req.MType, req.ID, req.Value)
+            if err != nil {
+                logger.Log.Debug("error while updating value", zap.Error(err))
+                w.WriteHeader(http.StatusInternalServerError)
+                return
+            }
+        } else {
+            err := app.storage.Add(ctx, req.MType, req.ID, req.Value)
+            if err != nil {
+                logger.Log.Debug("error while adding value", zap.Error(err))
+                w.WriteHeader(http.StatusInternalServerError)
+                return
+            }
+        }
+    case config.CountType:
+        if app.storage.Exist(ctx, req.MType, req.ID) {
+            err := app.storage.Update(ctx, req.MType, req.ID, req.Value)
+            if err != nil {
+                logger.Log.Debug("error while updating value", zap.Error(err))
+                w.WriteHeader(http.StatusInternalServerError)
+                return
+            }
+        } else {
+            err := app.storage.Add(ctx, req.MType, req.ID, req.Value)
+            if err != nil {
+                logger.Log.Debug("error while adding value", zap.Error(err))
+                w.WriteHeader(http.StatusInternalServerError)
+                return
+            }
+        }
+    }
+    resp := models.Metrics{
+        ID:    req.ID,
+        MType: req.MType,
+        Value: req.Value,
+        Delta: req.Delta,
+    }
+    enc := json.NewEncoder(w)
+    if err := enc.Encode(resp); err != nil {
+        logger.Log.Debug("error encoding response", zap.Error(err))
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    logger.Log.Debug("sending HTTP 200 response")
+}
+
+func (app *app) getAllmetrics(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    w.Header().Add("Content-Encoding", "gzip")
+    metrics, err := app.storage.GetAll(r.Context())
+    if err != nil {
+        logger.Log.Debug("error", zap.Error(err))
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    if err := t.Execute(w, metrics); err != nil {
+        logger.Log.Debug("error executing template", zap.Error(err))
+    }
+}
+
+func (app *app) getMetric(w http.ResponseWriter, r *http.Request) {
+    ctx, cancel := context.WithCancel(r.Context())
+    defer cancel()
+    if r.Method != http.MethodPost {
+        logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
+        w.WriteHeader(http.StatusMethodNotAllowed)
+        return
+    }
+    logger.Log.Debug("decoding request")
+    var req models.Metrics
+    dec := json.NewDecoder(r.Body)
+    if err := dec.Decode(&req); err != nil {
+        logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    resp := models.Metrics{
+        ID:    req.ID,
+        MType: req.MType,
+    }
+    switch req.MType {
+    case config.GaugeType:
+        if app.storage.Exist(ctx, req.MType, req.ID) {
+            res, err := app.storage.Get(ctx, req.MType, req.ID)
+            if err != nil {
+                logger.Log.Debug("error while obtaining metric", zap.Error(err))
+                w.WriteHeader(http.StatusInternalServerError)
+                return
+            }
+            resp.Value = res.Value
+        } else {
+            logger.Log.Debug("usupported metric name", zap.String("name", req.ID))
+            w.WriteHeader(http.StatusNotFound)
+            return
+        }
+    case config.CountType:
+        if app.storage.Exist(ctx, req.MType, req.ID) {
+            res, err := app.storage.Get(ctx, req.MType, req.ID)
+            if err != nil {
+                logger.Log.Debug("error while obtaining metric", zap.Error(err))
+                w.WriteHeader(http.StatusInternalServerError)
+                return
+            }
+            resp.Delta = res.Delta
+        } else {
+            logger.Log.Debug("usupported metric name", zap.String("name", req.ID))
+            w.WriteHeader(http.StatusNotFound)
+            return
+        }
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Header().Add("Content-Encoding", "gzip")
+    enc := json.NewEncoder(w)
+    if err := enc.Encode(resp); err != nil {
+        logger.Log.Debug("error encoding response", zap.Error(err))
+        return
+    }
+    logger.Log.Debug("sending HTTP 200 response")
+}
+
--- internal/storage/repository.go
+++ internal/storage/repository.go
@@ -1,6 +1,204 @@
 package storage
 
 import (
-	_ "github.com/jackc/pgx/v5"
+	"context"
+	"database/sql"
+	"net/http"
+
+	config "github.com/igortoigildin/go-metrics-altering/config/server"
+	"github.com/igortoigildin/go-metrics-altering/internal/logger"
+	"github.com/igortoigildin/go-metrics-altering/internal/models"
+	"go.uber.org/zap"
 )
+
+const (
+    GaugeType = "gauge"
+    CountType = "counter"
+    PollCount = "PollCount"
+)
+
+type Repository struct {
+    conn *sql.DB
+}
+
+func NewRepository(conn *sql.DB) *Repository {
+    return &Repository{
+        conn: conn,
+    }
+}
+
+func InitPostgresRepo(c context.Context, cfg *config.ConfigServer) *Repository {
+    dbDSN := cfg.FlagDBDSN
+    conn, err := sql.Open("pgx", dbDSN)
+    if err != nil {
+        logger.Log.Fatal("error while connecting to DB", zap.Error(err))
+    }
+    rep := NewRepository(conn)
+    ctx, cancel := context.WithCancel(c)
+    defer cancel()
+    // check connection
+    if err = rep.conn.PingContext(ctx); err != nil {
+        logger.Log.Fatal("error while connecting to DB", zap.Error(err))
+    }
+    // start Tx
+    tx, err := rep.conn.BeginTx(ctx, nil)
+    if err != nil {
+        logger.Log.Fatal("error while starting Tx", zap.Error(err))
+    }
+    defer tx.Rollback()
+    tx.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS counters (id SERIAL PRIMARY KEY, name TEXT NOT NULL,"+
+        "type TEXT NOT NULL, value bigint);")
+    tx.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS gauges (id SERIAL PRIMARY KEY, name TEXT NOT NULL,"+
+        "type TEXT NOT NULL, value DOUBLE PRECISION);") 
+    tx.Commit()
+    return rep
+}
+
+func (rep *Repository) Exist(ctx context.Context, metricType string, metricName string) bool {
+    switch metricType {
+    case GaugeType:
+        var exists bool
+        row := rep.conn.QueryRowContext(ctx, "SELECT EXISTS(SELECT 1 FROM gauges WHERE name = $1)", metricName)
+        if err := row.Scan(&exists); err != nil {
+            logger.Log.Fatal("error while checking existence", zap.Error(err))
+        }
+        if exists {
+            return true
+        }
+
+    case CountType:
+        var exists bool
+        row := rep.conn.QueryRowContext(ctx, "SELECT EXISTS(SELECT 1 FROM counters WHERE name = $1)", metricName)
+        if err := row.Scan(&exists); err != nil {
+            logger.Log.Fatal("error while checking existence", zap.Error(err))
+        }
+        if exists {
+            return true
+        }
+    }
+    return false
+}
+
+func (rep *Repository) Add(ctx context.Context, metricType string, metricName string, metricValue any) error {
+    switch metricType {
+    case GaugeType:
+        _, err := rep.conn.ExecContext(ctx, "INSERT INTO gauges(name, type, value) VALUES($1, $2, $3)", metricName, GaugeType, metricValue)
+        if err != nil {
+            logger.Log.Fatal("error while saving gauge metric to the db", zap.Error(err))
+            return err
+        }
+    case CountType:
+        _, err := rep.conn.ExecContext(ctx, "INSERT INTO counters(name, type, value) VALUES($1, $2, $3)", metricName, CountType, metricValue)
+        if err != nil {
+            logger.Log.Fatal("error while saving counter metric to the db", zap.Error(err))
+            return err
+        }
+    }
+    return nil
+}
+
+func (rep *Repository) Update(ctx context.Context, metricType string, metricName string, metricValue any) error {
+    switch metricType {
+    case GaugeType:
+        _, err := rep.conn.ExecContext(ctx, "UPDATE gauges SET value = $1 WHERE name = $2", metricValue, metricName)
+        if err != nil {
+            logger.Log.Fatal("error while updating counter metric", zap.Error(err))
+            return err
+        }
+    case CountType:
+        _, err := rep.conn.ExecContext(ctx, "UPDATE counters SET value = value + $1 WHERE name = $2", metricValue, metricName)
+        if err != nil {
+            logger.Log.Fatal("error while saving counter metric to the db", zap.Error(err))
+            return err
+        }
+    }
+    return nil
+}
+
+func (rep *Repository) Get(ctx context.Context, metricType string, metricName string) (models.Metrics, error) {
+    var metric models.Metrics
+    switch metricType {
+    case GaugeType:
+        var metric models.Metrics
+        err := rep.conn.QueryRowContext(ctx, "SELECT name, type, value FROM gauges WHERE name = $1", metricName).Scan(
+            &metric.ID, &metric.MType, &metric.Value)
+        switch {
+        case err == sql.ErrNoRows:
+            logger.Log.Fatal("no rows", zap.Error(err))
+            return metric, err
+        case err != nil:
+            logger.Log.Fatal("error while obtaining metrics", zap.Error(err))
+            return metric, err
+        default:
+            return metric, nil
+        }
+    case CountType:
+        var metric models.Metrics
+        err := rep.conn.QueryRowContext(ctx, "SELECT name, type, value FROM counters WHERE name = $1", metricName).Scan(
+            &metric.ID, &metric.MType, &metric.Delta)
+        switch {
+        case err == sql.ErrNoRows:
+            logger.Log.Fatal("no rows", zap.Error(err))
+            return metric, err
+        case err != nil:
+            logger.Log.Fatal("error while obtaining metrics", zap.Error(err))
+            return metric, err
+        default:
+            return metric, nil
+        }
+    }
+    return metric, nil
+}
+
+func (rep *Repository) Ping(w http.ResponseWriter, r *http.Request) {
+    ctx := r.Context()
+    if err := rep.conn.PingContext(ctx); err != nil {
+        logger.Log.Info("error", zap.Error(err))
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    rep.conn.Close()
+    w.WriteHeader(http.StatusOK)
+}
+
+func (rep *Repository) GetAll(ctx context.Context) (map[string]any, error) {
+    metrics := make(map[string]any, 33)
+    rows, err := rep.conn.QueryContext(ctx, "SELECT name, value FROM gauges WHERE type = $1", GaugeType)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+    for rows.Next() {
+        var name string
+        var value any
+        err = rows.Scan(&name, &value)
+        if err != nil {
+            return nil, err
+        }
+        metrics[name] = value
+    }
+    err = rows.Err()
+    if err != nil {
+        return nil, err
+    }
+    rows, err = rep.conn.QueryContext(ctx, "SELECT name, value FROM counters WHERE type = $1", CountType)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+    for rows.Next() {
+        var name string
+        var value any
+        err = rows.Scan(&name, &value)
+        if err != nil {
+            return nil, err
+        }
+        metrics[name] = value
+    }
+    err = rows.Err()
+    if err != nil {
+        return nil, err
+    }
+    return metrics, nil
+}
 
