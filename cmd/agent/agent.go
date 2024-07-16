8df44304e4d4270819daeafbf7ed8b4d6a5fda39
Igor Toigildin <toygildinigor@gmail.com>
2024-07-16 12:45:41 +0300

    upd

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"strconv"
 	"time"
 
@@ -54,7 +53,6 @@ func main() {
 			metric.Delta = &delta
 			metrics = append(metrics, metric)
 			metricsJSON, err := json.Marshal(metrics)
-			fmt.Println(metricsJSON)
 			if err != nil {
 				logger.Log.Debug("unexpected sending metric error:", zap.Error(err))
 			}
