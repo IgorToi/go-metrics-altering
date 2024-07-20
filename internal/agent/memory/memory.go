94115d022846aad8ac700c1a5067de06d9e740b2
Igor Toigildin <toygildinigor@gmail.com>
2024-07-20 15:14:04 +0300

    Agent refactoring

--- internal/agent/memory/memory.go
+++ internal/agent/memory/memory.go
@@ -10,58 +10,59 @@ import (
 )
 
 var (
-	ErrConnectionFailed = errors.New("connection failed")
+    ErrConnectionFailed = errors.New("connection failed")
 )
 
 type MemoryStats struct {
-	GaugeMetrics 		map[string]float64
-	CounterMetric		int
-	Rtm                runtime.MemStats
+    GaugeMetrics        map[string]float64
+    CounterMetric       int
+    Rtm                runtime.MemStats
 }
 
 func NewMemoryStats() *MemoryStats{
-	return &MemoryStats{
-		GaugeMetrics: make(map[string]float64),
-	}
+    return &MemoryStats{
+        GaugeMetrics: make(map[string]float64),
+    }
 }
 
-func (c *MemoryStats) UpdateMetrics(cfg *config.ConfigAgent) {
-	PauseDuration := time.Duration(cfg.FlagPollInterval) * time.Second
-	for {
-		time.Sleep(PauseDuration)
-		runtime.ReadMemStats(&c.Rtm)
-		c.GaugeMetrics["Alloc"] = float64(c.Rtm.Alloc)
-		c.GaugeMetrics["BuckHashSys"] = float64(c.Rtm.BuckHashSys)
-		c.GaugeMetrics["Frees"] = float64(c.Rtm.Frees)
-		c.GaugeMetrics["GCCPUFraction"] = float64(c.Rtm.GCCPUFraction)
-		c.GaugeMetrics["GCSys"] = float64(c.Rtm.GCSys)
-		c.GaugeMetrics["HeapAlloc"] = float64(c.Rtm.HeapAlloc)
-		c.GaugeMetrics["HeapIdle"] = float64(c.Rtm.HeapIdle)
-		c.GaugeMetrics["HeapInuse"] = float64(c.Rtm.HeapInuse)
-		c.GaugeMetrics["HeapObjects"] = float64(c.Rtm.HeapObjects)
-		c.GaugeMetrics["HeapReleased"] = float64(c.Rtm.HeapReleased)
-		c.GaugeMetrics["HeapSys"] = float64(c.Rtm.HeapSys)
-		c.GaugeMetrics["LastGC"] = float64(c.Rtm.LastGC)
-		c.GaugeMetrics["Lookups"] = float64(c.Rtm.Lookups)
-		c.GaugeMetrics["MCacheInuse"] = float64(c.Rtm.MCacheInuse)
-		c.GaugeMetrics["MCacheSys"] = float64(c.Rtm.MCacheSys)
-		c.GaugeMetrics["MSpanInuse"] = float64(c.Rtm.MSpanInuse)
-		c.GaugeMetrics["MSpanSys"] = float64(c.Rtm.MSpanSys)
-		c.GaugeMetrics["Mallocs"] = float64(c.Rtm.Mallocs)
-		c.GaugeMetrics["NextGC"] = float64(c.Rtm.NextGC)
-		c.GaugeMetrics["NumForcedGC"] = float64(c.Rtm.NumForcedGC)
-		c.GaugeMetrics["NumGC"] = float64(c.Rtm.NumGC)
-		c.GaugeMetrics["OtherSys"] = float64(c.Rtm.OtherSys)
-		c.GaugeMetrics["NextGC"] = float64(c.Rtm.NextGC)
-		c.GaugeMetrics["NumForcedGC"] = float64(c.Rtm.NumForcedGC)
-		c.GaugeMetrics["NumGC"] = float64(c.Rtm.NumGC)
-		c.GaugeMetrics["OtherSys"] = float64(c.Rtm.OtherSys)
-		c.GaugeMetrics["PauseTotalNs"] = float64(c.Rtm.PauseTotalNs)
-		c.GaugeMetrics["StackInuse"] = float64(c.Rtm.StackInuse)
-		c.GaugeMetrics["StackSys"] = float64(c.Rtm.StackSys)
-		c.GaugeMetrics["Sys"] = float64(c.Rtm.StackSys)
-		c.GaugeMetrics["TotalAlloc"] = float64(c.Rtm.TotalAlloc)
-		c.GaugeMetrics["RandomValue"] = rand.Float64()
-		c.CounterMetric++
-	}
+func (m *MemoryStats) UpdateMetrics(cfg *config.ConfigAgent) {
+    PauseDuration := time.Duration(cfg.FlagPollInterval) * time.Second
+    for {
+        time.Sleep(PauseDuration)
+        runtime.ReadMemStats(&m.Rtm)
+        m.GaugeMetrics["Alloc"] = float64(m.Rtm.Alloc)
+        m.GaugeMetrics["BuckHashSys"] = float64(m.Rtm.BuckHashSys)
+        m.GaugeMetrics["Frees"] = float64(m.Rtm.Frees)
+        m.GaugeMetrics["GCCPUFraction"] = float64(m.Rtm.GCCPUFraction)
+        m.GaugeMetrics["GCSys"] = float64(m.Rtm.GCSys)
+        m.GaugeMetrics["HeapAlloc"] = float64(m.Rtm.HeapAlloc)
+        m.GaugeMetrics["HeapIdle"] = float64(m.Rtm.HeapIdle)
+        m.GaugeMetrics["HeapInuse"] = float64(m.Rtm.HeapInuse)
+        m.GaugeMetrics["HeapObjects"] = float64(m.Rtm.HeapObjects)
+        m.GaugeMetrics["HeapReleased"] = float64(m.Rtm.HeapReleased)
+        m.GaugeMetrics["HeapSys"] = float64(m.Rtm.HeapSys)
+        m.GaugeMetrics["LastGC"] = float64(m.Rtm.LastGC)
+        m.GaugeMetrics["Lookups"] = float64(m.Rtm.Lookups)
+        m.GaugeMetrics["MCacheInuse"] = float64(m.Rtm.MCacheInuse)
+        m.GaugeMetrics["MCacheSys"] = float64(m.Rtm.MCacheSys)
+        m.GaugeMetrics["MSpanInuse"] = float64(m.Rtm.MSpanInuse)
+        m.GaugeMetrics["MSpanSys"] = float64(m.Rtm.MSpanSys)
+        m.GaugeMetrics["Mallocs"] = float64(m.Rtm.Mallocs)
+        m.GaugeMetrics["NextGC"] = float64(m.Rtm.NextGC)
+        m.GaugeMetrics["NumForcedGC"] = float64(m.Rtm.NumForcedGC)
+        m.GaugeMetrics["NumGC"] = float64(m.Rtm.NumGC)
+        m.GaugeMetrics["OtherSys"] = float64(m.Rtm.OtherSys)
+        m.GaugeMetrics["NextGC"] = float64(m.Rtm.NextGC)
+        m.GaugeMetrics["NumForcedGC"] = float64(m.Rtm.NumForcedGC)
+        m.GaugeMetrics["NumGC"] = float64(m.Rtm.NumGC)
+        m.GaugeMetrics["OtherSys"] = float64(m.Rtm.OtherSys)
+        m.GaugeMetrics["PauseTotalNs"] = float64(m.Rtm.PauseTotalNs)
+        m.GaugeMetrics["StackInuse"] = float64(m.Rtm.StackInuse)
+        m.GaugeMetrics["StackSys"] = float64(m.Rtm.StackSys)
+        m.GaugeMetrics["Sys"] = float64(m.Rtm.StackSys)
+        m.GaugeMetrics["TotalAlloc"] = float64(m.Rtm.TotalAlloc)
+        m.GaugeMetrics["RandomValue"] = rand.Float64()
+        m.CounterMetric++
+    }
 }
+
