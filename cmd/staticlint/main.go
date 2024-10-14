ce9180e48b52925454482ee8238651d8ce1ef408
Igor Toigildin <toygildinigor@gmail.com>
2024-10-14 11:15:37 +0300

    check test

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -1,65 +1,47 @@
 package main
 
-import (
-	"encoding/json"
-	"os"
-	"path/filepath"
+// // Config — имя файла конфигурации.
+// const Config = `config.json`
 
-	"github.com/igortoigildin/go-metrics-altering/cmd/staticlint/checker"
-	"github.com/kisielk/errcheck/errcheck"
-	"honnef.co/go/tools/unused"
+// // ConfigData описывает структуру файла конфигурации.
+// type ConfigData struct {
+// 	Staticcheck []string
+// }
 
-	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/multichecker"
-	"golang.org/x/tools/go/analysis/passes/nilness"
-	"golang.org/x/tools/go/analysis/passes/printf"
-	"golang.org/x/tools/go/analysis/passes/shadow"
-	"golang.org/x/tools/go/analysis/passes/structtag"
-	"honnef.co/go/tools/staticcheck"
-)
+// func main() {
+// 	appfile, err := os.Executable()
 
-// Config — имя файла конфигурации.
-const Config = `config.json`
-
-// ConfigData описывает структуру файла конфигурации.
-type ConfigData struct {
-	Staticcheck []string
-}
-
-func main() {
-	appfile, err := os.Executable()
-
-	if err != nil {
-		panic(err)
-	}
-	data, err := os.ReadFile(filepath.Join(filepath.Dir(appfile), Config))
-	if err != nil {
-		panic(err)
-	}
-	var cfg ConfigData
-	if err = json.Unmarshal(data, &cfg); err != nil {
-		panic(err)
-	}
-	mychecks := []*analysis.Analyzer{
-		checker.ExitCheckAnalyzer,
-		printf.Analyzer,
-		shadow.Analyzer,
-		structtag.Analyzer,
-		nilness.Analyzer,
-		errcheck.Analyzer,
-		unused.Analyzer.Analyzer,
-	}
-	checks := make(map[string]bool)
-	for _, v := range cfg.Staticcheck {
-		checks[v] = true
-	}
-	// добавляем анализаторы из staticcheck, которые указаны в файле конфигурации
-	for _, v := range staticcheck.Analyzers {
-		if checks[v.Analyzer.Name] {
-			mychecks = append(mychecks, v.Analyzer)
-		}
-	}
-	multichecker.Main(
-		mychecks...,
-	)
-}
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	data, err := os.ReadFile(filepath.Join(filepath.Dir(appfile), Config))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	var cfg ConfigData
+// 	if err = json.Unmarshal(data, &cfg); err != nil {
+// 		panic(err)
+// 	}
+// 	mychecks := []*analysis.Analyzer{
+// 		checker.ExitCheckAnalyzer,
+// 		printf.Analyzer,
+// 		shadow.Analyzer,
+// 		structtag.Analyzer,
+// 		nilness.Analyzer,
+// 		errcheck.Analyzer,
+// 		unused.Analyzer.Analyzer,
+// 	}
+// 	checks := make(map[string]bool)
+// 	for _, v := range cfg.Staticcheck {
+// 		checks[v] = true
+// 	}
+// 	// добавляем анализаторы из staticcheck, которые указаны в файле конфигурации
+// 	for _, v := range staticcheck.Analyzers {
+// 		if checks[v.Analyzer.Name] {
+// 			mychecks = append(mychecks, v.Analyzer)
+// 		}
+// 	}
+// 	multichecker.Main(
+// 		mychecks...,
+// 	)
+// }
