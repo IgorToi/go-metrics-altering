87dd1caa7e77fb585cdf30dd8d6deaf0491dc90a
Igor Toigildin <toygildinigor@gmail.com>
2024-11-17 14:18:32 +0300

    add auth middleware which verifies that agent IP is in trusted subnet

--- internal/server/api/router.go
+++ internal/server/api/router.go
@@ -6,6 +6,7 @@ import (
 	"text/template"
 
 	config "github.com/igortoigildin/go-metrics-altering/config/server"
+	"github.com/igortoigildin/go-metrics-altering/pkg/middlewares/auth"
 	"github.com/igortoigildin/go-metrics-altering/pkg/middlewares/compress"
 	"github.com/igortoigildin/go-metrics-altering/pkg/middlewares/logging"
 	"github.com/igortoigildin/go-metrics-altering/pkg/middlewares/timeout"
@@ -18,13 +19,13 @@ func Router(ctx context.Context, cfg *config.ConfigServer, storage Storage) *htt
 	t = templates.ParseTemplate()
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /value/{metricType}/{metricName}", logging.WithLogging(compress.GzipMiddleware((http.HandlerFunc(valuePathHandler(storage))))))
-	mux.HandleFunc("POST /update/{metricType}/{metricName}/{metricValue}", logging.WithLogging(compress.GzipMiddleware((http.HandlerFunc(updatePathHandler(storage))))))
-	mux.HandleFunc("GET /ping", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware(http.HandlerFunc(ping(storage))))))
-	mux.HandleFunc("GET /", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware((http.HandlerFunc(getAllmetrics(storage)))))))
-	mux.HandleFunc("POST /updates/", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware((http.HandlerFunc(updates(storage)))))))
-	mux.HandleFunc("POST /value/", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware((http.HandlerFunc(getMetric(storage)))))))
-	mux.HandleFunc("POST /update/", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware((http.HandlerFunc(updateMetric(storage)))))))
+	mux.HandleFunc("GET /value/{metricType}/{metricName}", logging.WithLogging(compress.GzipMiddleware((auth.Auth(http.HandlerFunc(valuePathHandler(storage)), cfg)))))
+	mux.HandleFunc("POST /update/{metricType}/{metricName}/{metricValue}", logging.WithLogging(compress.GzipMiddleware(auth.Auth(http.HandlerFunc(updatePathHandler(storage)), cfg))))
+	mux.HandleFunc("GET /ping", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware(auth.Auth(http.HandlerFunc(ping(storage)), cfg)))))
+	mux.HandleFunc("GET /", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware(auth.Auth(http.HandlerFunc(getAllmetrics(storage)), cfg)))))
+	mux.HandleFunc("POST /updates/", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware(auth.Auth(http.HandlerFunc(updates(storage)), cfg)))))
+	mux.HandleFunc("POST /value/", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware(auth.Auth(http.HandlerFunc(getMetric(storage)), cfg)))))
+	mux.HandleFunc("POST /update/", timeout.Timeout(cfg.ContextTimout, logging.WithLogging(compress.GzipMiddleware(auth.Auth(http.HandlerFunc(updateMetric(storage)), cfg)))))
 
 	return mux
 }
--- pkg/middlewares/auth/auth.go
+++ pkg/middlewares/auth/auth.go
@@ -5,7 +5,69 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"net"
+	"net/http"
+
+	config "github.com/igortoigildin/go-metrics-altering/config/server"
+	"github.com/igortoigildin/go-metrics-altering/pkg/logger"
+	"go.uber.org/zap"
 )
+
+// Auth middleware checks whether IP request is in trusted subnet.
+func Auth(next http.HandlerFunc, cfg *config.ConfigServer) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if cfg.FlagTrustedSubnet != "" {
+			ipStr := r.Header.Get("X-Real-IP")
+			if ipStr == "" {
+				logger.Log.Info("X-Real-IP header not stated")
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			
+			// check whether X-Real-IP is the correct IP-address
+			ip := net.ParseIP(ipStr)
+			if ip == nil {
+				logger.Log.Info("Invalid IP address in X-Real-IP")
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			isTrusted, err := isIPInTrustedSubnet(ipStr, cfg.FlagTrustedSubnet)
+			if err != nil {
+				logger.Log.Error("error while validating IP address", zap.Error(err))
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			if isTrusted {
+				logger.Log.Info("IP is in the trusted subnet")
+			} else {
+				logger.Log.Info("IP is NOT in the trusted subnet")
+				w.WriteHeader(http.StatusForbidden)
+				return
+			}
+		}
+		next(w, r)
+	})
+}
+
+func isIPInTrustedSubnet(ipStr string, trustedSubnet string) (bool, error) {
+	ip := net.ParseIP(ipStr)
+	if ip == nil {
+		logger.Log.Error("invalid IP address")
+		return false, fmt.Errorf("invalid IP address: %s", ipStr)
+	}
+
+	// converting trusted subnet to net.IPNet
+	_, trustedNet, err := net.ParseCIDR(trustedSubnet)
+	if err != nil {
+		return false, fmt.Errorf("invalid trusted subnet: %s", trustedSubnet)
+	}
+
+	// chesk whether ip is in trusted subnet
+	return trustedNet.Contains(ip), nil
+}
 
 // ValidMAC reports whether messageMAC is a valid HMAC tag for message.
 func ValidMAC(message, messageMAC, key []byte) (bool, error) {
